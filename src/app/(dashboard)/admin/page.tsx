f0272c82a3dc091660b5d9c2c4a5fe2ffaea764b
OP <106511969+Op-Pautu@users.noreply.github.com>
2025-02-18 00:01:36 +0530

    Add dynamic user count cards to admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -7,41 +7,41 @@ import { UserCards } from "@/components/user-cards";
 import React from "react";
 
 const AdminPage = () => {
-	return (
-		<div className="p-4 flex gap-4 flex-col md:flex-row">
-			{/* Left */}
-			<div className="w-full lg:w-2/3 flex flex-col gap-8">
-				{/* User Cards */}
-				<div className="flex gap-4 justify-between flex-wrap">
-					<UserCards type="student" />
-					<UserCards type="teacher" />
-					<UserCards type="parent" />
-					<UserCards type="staff" />
-				</div>
-				{/* Middle Charts */}
-				<div className="flex gap-4 flex-col lg:flex-row">
-					{/* Count Chart */}
-					<div className="w-full lg:w-1/3 h-[450px]">
-						<CountChart />
-					</div>
-					{/* Attendance Chart */}
-					<div className="w-full lg:w-2/3 h-[450px]">
-						<AttendanceChart />
-					</div>
-				</div>
-				{/* Bottom Chart */}
-				<div className="w-full h-[500px]">
-					<FinanceChart />
-				</div>
-			</div>
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCards type="admin" />
+          <UserCards type="teacher" />
+          <UserCards type="student" />
+          <UserCards type="parent" />
+        </div>
+        {/* Middle Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* Bottom Chart */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
 
-			{/* Right */}
-			<div className="w-full lg:w-1/3 flex flex-col gap-8">
-				<EventCalendar />
-				<Announcements />
-			</div>
-		</div>
-	);
+      {/* Right */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
